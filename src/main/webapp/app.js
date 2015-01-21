82e49c41d59c73f552d8393dfcffa0f0fe2a476e
Niels Dommerholt <niels.dommerholt@jdriven.com>
2015-01-21 21:46:55 +0100

    Created simple Angular demo app

---
+++ src/main/webapp/app.js
@@ -0,0 +1,58 @@
+var appModule = angular.module('myApp', []); 
+
+appModule.controller('MainCtrl', ['mainService','$scope','$http',
+        function(mainService, $scope, $http) {
+            $scope.greeting = 'Welcome to the JSON Web Token / AngularJR / Spring example!';
+            $scope.token = null;
+            $scope.error = null;
+            $scope.roleUser = false;
+            $scope.roleAdmin = false;
+            $scope.roleFoo = false;
+ 
+            $scope.login = function() {
+            	$scope.error = null;
+            	mainService.login($scope.userName).then(function(token) {
+            		$scope.token = token;
+            		$http.defaults.headers.common.Authorization = 'Bearer ' + token;
+            		$scope.checkRoles();
+            	},
+            	function(error){
+            		$scope.error = error
+            		$scope.userName = '';
+            	});
+            }
+            
+            $scope.checkRoles = function() {
+            	mainService.hasRole('user').then(function(user) {$scope.roleUser = user});
+            	mainService.hasRole('admin').then(function(admin) {$scope.roleAdmin = admin});
+            	mainService.hasRole('foo').then(function(foo) {$scope.roleFoo = foo});
+            }
+            
+            $scope.logout = function() {
+            	$scope.userName = '';
+            	$scope.token = null;
+            	$http.defaults.headers.common.Authorization = '';
+            }
+            
+            $scope.loggedIn = function() {
+            	return $scope.token !== null;
+            }
+        } ]);
+
+
+
+appModule.service('mainService', function($http) {
+    return {
+        login : function(username) {
+            return $http.post('/user/login', {name: username}).then(function(response) {
+                return response.data.token;
+            });
+        },
+        
+        hasRole : function(role) {
+        	return $http.get('/api/role/' + role).then(function(response){
+        		return response.data;
+        	});
+        }
+    };
+});
