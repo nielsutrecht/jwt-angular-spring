5ec44100150540445157fd460b8666cb1e41e934
Niels Dommerholt <niels@holmes.nl>
2015-01-28 12:24:48 +0100

    Added fat jar building to pom

--- src/main/java/com/nibado/example/jwtangspr/UserController.java
+++ src/main/java/com/nibado/example/jwtangspr/UserController.java
@@ -1,8 +1,5 @@
 package com.nibado.example.jwtangspr;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-
 import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
@@ -16,40 +13,43 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
-	private final Map<String, List<String>> userDb = new HashMap<>();
+    private final Map<String, List<String>> userDb = new HashMap<>();
 
-	public UserController() {
-		userDb.put("tom", Arrays.asList("user"));
-		userDb.put("sally", Arrays.asList("user", "admin"));
-	}
+    public UserController() {
+        userDb.put("tom", Arrays.asList("user"));
+        userDb.put("sally", Arrays.asList("user", "admin"));
+    }
 
-	@RequestMapping(value = "login", method = RequestMethod.POST)
-	public LoginResponse login(@RequestBody final UserLogin login)
-			throws ServletException {
-		if (login.name == null || !userDb.containsKey(login.name)) {
-			throw new ServletException("Invalid login");
-		}
-		return new LoginResponse(Jwts.builder().setSubject(login.name)
-				.claim("roles", userDb.get(login.name)).setIssuedAt(new Date())
-				.signWith(SignatureAlgorithm.HS256, "secretkey").compact());
-	}
+    @RequestMapping(value = "login", method = RequestMethod.POST)
+    public LoginResponse login(@RequestBody final UserLogin login)
+        throws ServletException {
+        if (login.name == null || !userDb.containsKey(login.name)) {
+            throw new ServletException("Invalid login");
+        }
+        return new LoginResponse(Jwts.builder().setSubject(login.name)
+            .claim("roles", userDb.get(login.name)).setIssuedAt(new Date())
+            .signWith(SignatureAlgorithm.HS256, "secretkey").compact());
+    }
 
-	@SuppressWarnings("unused")
-	private static class UserLogin {
-		public String name;
-		public String password;
-	}
+    @SuppressWarnings("unused")
+    private static class UserLogin {
+        public String name;
+        public String password;
+    }
 
-	@SuppressWarnings("unused")
-	private static class LoginResponse {
-		public String token;
+    @SuppressWarnings("unused")
+    private static class LoginResponse {
+        public String token;
 
-		public LoginResponse(final String token) {
-			this.token = token;
-		}
-	}
+        public LoginResponse(final String token) {
+            this.token = token;
+        }
+    }
 }
--- src/main/java/com/nibado/example/jwtangspr/WebApplication.java
+++ src/main/java/com/nibado/example/jwtangspr/WebApplication.java
@@ -2,34 +2,26 @@ package com.nibado.example.jwtangspr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.embedded.FilterRegistrationBean;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.Configuration;
 
-@RestController
 @EnableAutoConfiguration
 @ComponentScan
-public class WebApplication extends SpringBootServletInitializer {
-	@Override
-	protected SpringApplicationBuilder configure(
-			final SpringApplicationBuilder application) {
-		return application;
-	}
+@Configuration
+public class WebApplication {
+    @Bean
+    public FilterRegistrationBean jwtFilter() {
+        final FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+        registrationBean.setFilter(new JwtFilter());
+        registrationBean.addUrlPatterns("/api/*");
 
-	@Bean
-	public FilterRegistrationBean jwtFilter() {
-		final FilterRegistrationBean registrationBean = new FilterRegistrationBean();
-		registrationBean.setFilter(new JwtFilter());
-		registrationBean.addUrlPatterns("/api/*");
+        return registrationBean;
+    }
 
-		return registrationBean;
-	}
-
-	public static void main(final String[] args) throws Exception {
-		SpringApplication.run(WebApplication.class, args);
-	}
+    public static void main(final String[] args) throws Exception {
+        SpringApplication.run(WebApplication.class, args);
+    }
 
 }
