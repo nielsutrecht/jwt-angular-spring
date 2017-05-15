3b3e578a9d700aadf833a4cf07d270f256f79ccb
Niels Dommerholt <ndommerholt@gmail.com>
2017-05-15 17:15:12 +0200

    Upgraded to Spring Boot 1.5.3

--- src/main/java/com/nibado/example/jwtangspr/WebApplication.java
+++ src/main/java/com/nibado/example/jwtangspr/WebApplication.java
@@ -2,7 +2,7 @@ package com.nibado.example.jwtangspr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
